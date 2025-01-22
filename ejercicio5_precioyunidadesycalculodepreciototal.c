47c821a29bd8255a26a2fe388836adf487d0f032
sgonzalof <sergiogonzafeli@gmail>
2025-01-22 14:23:35 +0100

    ejercicio 5 sin acabar

---
+++ ejercicio5_precioyunidadesycalculodepreciototal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* code */
+    int  unidades;
+    float precio, total;
+    printf("Introduce el precio de un producto: ");
+    scanf("%f",precio);
+    printf("Introduce el número de unidades: ");
+    scanf("%d",unidades);
+    total = precio * unidades;
+    printf("El precio total de %d unidades a %f euros cada una es %f euros\n", unidades, precio, total);
+    return 0;
+    return 0;
+}
