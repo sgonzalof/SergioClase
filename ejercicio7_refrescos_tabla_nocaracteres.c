41e9ce687b9eca2cd929ed504eca634d2a059be0
sgonzalof <sergiogonzafeli@gmail>
2025-01-28 10:29:25 +0100

    ejercicio7_funciona pero no es una tabla de caracteres

---
+++ ejercicio7_refrescos_tabla_nocaracteres.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+    /*introduce por pantalla numero de refrescos que se vendieron y precio */
+    int c,n,l;
+    float pc, pn, pl, total;
+    printf("Introduce el numero de colas vendidas y su precio:");
+    scanf("%d %f", &c, &pc);
+    printf("Introduce el numero de refrescos de naranja vendidos y su precio:");
+    scanf("%d %f", &n, &pn);
+    printf("Introduce el numero de refrescos de limon vendidos y su precio:");
+    scanf( "%d %f", &l, &pl);
+
+    printf(" | PRODUCTO | VENTAS | PRECIO | TOTAL |\n");
+    printf("---------------------------------------\n");
+    printf(" | %6s | %6d | %6f | %6f |\n", "COLA", c, pc, c*pc);
+    printf(" | %4s | %6d | %6f | %6f |\n", "NARANJA", n, pn, n*pn);
+    printf(" | %6s | %6d | %6f | %6f |\n", "LIMON", l, pl, l*pl);
+    printf("---------------------------------------\n");
+    
+
+   
+    return 0;
+}
